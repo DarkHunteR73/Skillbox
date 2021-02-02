585d4faace2e49dfd1d088accf10f737726ba3da
DarkHunteR7 <darkstranger153@gmail.com>
2021-02-02 21:27:52 +0300

    Task-11 creared

---
+++ Module-11-homework/Task-11/Task-11.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    using namespace std;
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Введите два числа: ";
+    float number1, number2;
+    cin >> number1 >> number2;
+    cout << "Наибольшее число: " << fmax(number1, number2) << endl;
+    return 0;
+}
