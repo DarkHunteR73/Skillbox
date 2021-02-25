ff3e487e8c7efc11d928c9ce565594f79165fa6b
DarkHunteR7 <darkstranger153@gmail.com>
2021-02-26 01:47:53 +0300

    14.2, 14.3 tasks created

---
+++ Module-14/14.2 Function definition_4/14.2 Function definition_4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+float abs(float x)
+{
+    return x >= 0 ? x : -x;
+}
+
+float sin(float x)
+{
+    float result = 0.0;
+    float add = x;
+    int count = 0;
+
+    while (abs(add) >= 1e-6)
+    {
+        result += add;
+        ++count;
+        add *= (-1) * x * x / (2 * count) / (2 * count + 1);
+    } 
+    return result;
+}
+
+int main()
+{
+    std::cout << sin(3.1415);
+}
---
+++ Module-14/14.3 Simple numbers/14.3 Simple numbers.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+bool isPrime(int n)
+{
+    for (int d = 2; d * d <= n; ++d)
+    {
+        if (n % d == 0) return false;
+    }
+    return n > 1;
+}
+
+int main()
+{
+    std::cout << "Inpit numbers: ";
+    int a, b;
+    std::cin >> a >> b;
+
+    int count = 0;
+    for (size_t i = a; i <= b; ++i)
+    {
+        if (isPrime(i))
+        {
+            ++count;
+        }
+    }
+    std::cout << count << std::endl;
+}
