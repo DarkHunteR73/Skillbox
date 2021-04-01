69b696fa6535e385308d32c8a3cfbcf55697338a
DarkHunteR7 <darkstranger153@gmail.com>
2021-04-01 20:19:44 +0300

    repo reorganized. 16.2 task 1 created

---
+++ Module-16/16.1/Task-1/Task-1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "How many elements do you want? ";
+    int N;
+    std::cin >> N;
+
+    std::vector<int>vec(N);
+    int sum = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        std::cout << i + 1 << ". ";
+        std::cin >> vec[i];
+        sum += vec[i];
+    }
+
+    std::cout << "Average number is: " << static_cast<float>(sum) / N;
+}
+
+//Задание 1
+//Пользователь вводит с клавиатуры число n, а затем -- n целых чисел.
+//Необходимо вычислить их среднее арифметическое и вывести на экран.
---
+++ Module-16/16.1/Task-2/Task-2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "How many elements do you want? ";
+    int N;
+    std::cin >> N;
+
+    std::vector<float>vec(N);
+    for (int i = 0; i < N; ++i)
+    {
+        std::cout << i + 1 << ".: ";
+        std::cin >> vec[N - i - 1];
+    }
+
+    std::cout << std::endl;
+
+    //for (int i = 0; i < N / 2; ++i)
+    //{
+    //    std::swap(vec[i], vec[N - i - 1]);
+    //}
+
+    for (int i = 0; i < N; ++i)
+    {
+        std::cout << i + 1 << ". " << vec[i] << std::endl;
+    }
+}
+
+//Задание 2
+//Пользователь вводит с клавиатуры число n, а затем -- n действительных(дробных) чисел.Необходимо вывести их все в обратном порядке(то есть сначала то число,
+//которые пользователь ввёл последним и так далее).
