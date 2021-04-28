513a9c1a7c536e793f3e344669edc4b54cee4529
DarkHunteR7 <darkstranger153@gmail.com>
2021-04-29 00:11:38 +0300

    Quick sort algorithm

--- Module-15/Task-3/Task-3.cpp
+++ Module-15/Task-3/Task-3.cpp
@@ -1,9 +1,34 @@
 ﻿#include <iostream>
 #include <vector>
 
+// Крайне занятный алгоритм
+void quick_sort(std::vector<int>& vec, int left, int right)
+{
+	int i = left, j = right;
+	int pivot = vec[(left + right) / 2];
+
+	while (i <= j)
+	{
+		while (vec[i] < pivot) ++i;
+		while (vec[j] > pivot) --j;
+
+		if (i <= j)
+		{
+			std::swap(vec[i], vec[j]);
+			++i;
+			--j;
+		}
+	}
+
+	if (left < j)
+		quick_sort(vec, left, j);
+	if (i < right)
+		quick_sort(vec, i, right);
+}
+
 int main()
 {
-	std::vector<int> vec;
+	std::vector<int> vec{ 4,33,54,-7,56,81,-23,15,29,31,65,-90,118,554,99,112,421 };
 
 	while (true)
 	{
@@ -19,16 +44,18 @@ int main()
 			}
 			else
 			{
-				for (int i = 0; i < vec.size(); ++i)
-				{
-					for (int j = 0; j < vec.size() - i - 1; ++j)
-					{
-						if (vec[j] > vec[j + 1])
-						{
-							std::swap(vec[j], vec[j + 1]);
-						}
-					}
-				}
+				quick_sort(vec, 0, vec.size() - 1);
+
+				//for (int i = 0; i < vec.size(); ++i)
+				//{
+				//	for (int j = 0; j < vec.size() - i - 1; ++j)
+				//	{
+				//		if (vec[j] > vec[j + 1])
+				//		{
+				//			std::swap(vec[j], vec[j + 1]);
+				//		}
+				//	}
+				//}
 
 				std::cout << vec[4] << std::endl;
 			}
--- Module-15/Task-4/Task-4.cpp
+++ Module-15/Task-4/Task-4.cpp
@@ -1,20 +1,47 @@
 ﻿#include <iostream>
 #include <vector>
 
+// Крайне занятный алгоритм
+void quick_sort(std::vector<int>& vec, int left, int right)
+{
+	int i = left, j = right;
+	int pivot = std::abs(vec[(left + right) / 2]);
+
+	while (i <= j)
+	{
+		while (std::abs(vec[i]) < pivot) ++i;
+		while (std::abs(vec[j]) > pivot) --j;
+
+		if (i <= j)
+		{
+			std::swap(vec[i], vec[j]);
+			++i;
+			--j;
+		}
+	}
+
+	if (left < j)
+		quick_sort(vec, left, j);
+	if (i < right)
+		quick_sort(vec, i, right);
+}
+
 int main()
 {
 	std::vector<int> vec{ -100,-50, -5, 1, 10, 15 };
 
-	for (int i = 0; i < vec.size(); ++i)
-	{
-		for (int j = 0; j < vec.size() - i - 1; ++j)
-		{
-			if (std::abs(vec[j]) > std::abs(vec[j + 1]))
-			{
-				std::swap(vec[j], vec[j + 1]);
-			}
-		}
-	}
+	quick_sort(vec, 0, vec.size() - 1);
+
+	//for (int i = 0; i < vec.size(); ++i)
+	//{
+	//	for (int j = 0; j < vec.size() - i - 1; ++j)
+	//	{
+	//		if (std::abs(vec[j]) > std::abs(vec[j + 1]))
+	//		{
+	//			std::swap(vec[j], vec[j + 1]);
+	//		}
+	//	}
+	//}
 
 	for (int i = 0; i < vec.size(); ++i)
 	{
