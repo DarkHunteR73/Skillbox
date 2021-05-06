acecce678b1042c79f4f9933ce68d4bcf62a945f
DarkHunteR7 <darkstranger153@gmail.com>
2021-05-07 00:44:52 +0300

    16.4 Task-2 solved

---
+++ Module-16/16.4-Homework/Task-2/Task-2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Input integer and fractional part: ";
+    int intPart, fracPart;
+    std::cin >> intPart >> fracPart;
+
+    std::string tmp = std::to_string(intPart) + '.' + std::to_string(fracPart);
+
+    double result = std::stod(tmp);
+    std::cout << "The result is: " << result << std::endl;
+}
+
+//Сшиватель дробных чисел
+//
+//Что нужно сделать
+//
+//Надо написать небольшую программу.Из целой и дробной частей числа, которые в отдельности вводит пользователь надо составить 
+//новое число с плавающей точкой(типа double) и вывести это число обратно в консоль, для проверки.Целая часть - это та, часть числа, 
+//которая находится до точки(запятой), дробная - после.
