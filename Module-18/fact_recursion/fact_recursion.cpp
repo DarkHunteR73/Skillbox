7898aa880f519f10719e68381844197e41130421
DarkHunteR7 <darkstranger153@gmail.com>
2021-05-23 19:41:54 +0300

    recursion tasks

---
+++ Module-18/fact_recursion/fact_recursion.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <climits>
+
+uint64_t fact(uint64_t num)
+{
+	if (num == 0 || num == 1) return 1;
+	return num * fact(num - 1);
+}
+
+int main()
+{
+	for (int i = 0; i <=20; ++i)
+	{
+		std::cout << i << '\t' << fact(i) << std::endl;
+	}
+
+	std::cout << "\nMAX\t" << UINT64_MAX << std::endl;
+}
---
+++ Module-18/fib_recursion/fib_recursion.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+long long fib(int n)
+{
+	if (n == 1 || n == 2) return 1;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		return fib(n - 1) + fib(n - 2);
+	}
+}
+
+int main()
+{
+	std::cout << fib(20);
+}
