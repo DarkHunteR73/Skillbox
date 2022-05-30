02fd20a5417617cebea7e98663a53b5a469dfe90
DarkHunteR7 <darkstranger153@gmail.com>
2022-05-30 10:37:05 +0300

    35.6 Task-1 completed

---
+++ Module-35/Task-1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+void printIntegers(std::initializer_list<int> list) {
+    std::vector<int> vec{ list };
+
+    for (auto i : vec) {
+        std::cout << i << ' ';
+    }
+}
+
+int main() {
+    printIntegers({ 1, 2, 3, 4, 5 });
+    return 0;
+}
+
+//    Задание 1 (к уроку 2)
+//
+//
+//    Цель задания
+//
+//    Закрепить работу с нововведениями в базовых конструкциях языка.
+//
+//
+//    Что нужно сделать
+//
+//    Вспомните пример с range-based циклом по закрытию файлов:
+//
+//    for(auto& File : files)
+//    currentFile.close();
+//
+//    По аналогии распечатайте числа от 1 до 5, используя auto и initializer_list.
